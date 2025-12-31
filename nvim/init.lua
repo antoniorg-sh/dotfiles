f315e2ff52aca053f052426fb10dc4c6d65e4c67
socratech-sh <antonio.ruv73@gmail.com>
2025-12-31 16:01:25 -0600

    Initial config files for personal laptop

---
+++ nvim/init.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.opt.nu = true
+vim.opt.relativenumber = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.colorcolumn = ""
